639ca8262e28209645e8327698c6d7afcd00ea92
media <zwwang.china@gmail.com>
2016-06-14 15:04:17 -0400

    visua

--- RNN_ADNI_Analysis/DataVisualize.py
+++ RNN_ADNI_Analysis/DataVisualize.py
@@ -6,8 +6,15 @@ Given train, validation, test index.
 @Zhewei
 5/30/2016
 
-Randomly select train, validation and test index.
-6/9/2016 
+
+Label of NC:    0
+Label of AD:    1
+Label of EMCI:  2
+Label of LMCI:  3
+Label of SMC:   4
+
+@Zhewei
+6/14/2016 
 """
 
 import sys,os
@@ -22,7 +29,7 @@ from sklearn.feature_selection import chi2
 import matplotlib.pyplot as pyplot
 
 
-totalNo = 86
+totalNo = 189
 
 index = [i for i in range(totalNo)]
 shuffle(index)
@@ -42,20 +49,25 @@ def usage (programm):
     print ("usage: %s ..data/*Subj*.pickle.gz"%(programm))
     
 def work(fnames):
-    WholeData, WholeLabel = stackData(fnames, DataIndex)
-
+    wholeData, wholeLabel = stackData(fnames, DataIndex)
+    
+    print (len(DataIndex))
     print (wholeData.shape)
     sampleNo = wholeData.shape[0]
     timeStep = wholeData.shape[1]
     featureNo = wholeData.shape[2]
     
-    print (trainLabel.shape)
-    print (validationLabel.shape)
-    wholeLabel = np.append(trainLabel, validationLabel)
-    wholeLabel = np.append(wholeLabel, testLabel)
-    print (wholeLabel.shape)
-    print (np.amax(wholeData))
-    print (np.amin(wholeData))
+    
+    
+    # find one of AD, NC, EMCI, LMCI
+    wholeLabel = wholeLabel.tolist()
+
+    AD_index = wholeLabel.index(1)
+    NC_index = wholeLabel.index(0)
+    EMCI_index = wholeLabel.index(2)
+    LMCI_index = wholeLabel.index(3)
+    
+    print (AD_index, NC_index, EMCI_index, LMCI_index)
     
     
     
@@ -65,27 +77,40 @@ def work(fnames):
     '''
     Draw
     '''
-    iAD = 0
-    iNC = 0
-    iLM = 0
-    print (sampleNo)
-    for i in range(sampleNo):
-        if i < sampleNo:
-            tmp = wholeData[i,:,:]
-            tmp.reshape(timeStep,featureNo)
-            if wholeLabel[i] == 1: #AD
-                for j in range(timeStep):
-                    color = (0, (iAD+(Group1_No))/(Group1_No+(Group1_No)), 0)
-                    plotNC, = pyplot.plot(range(featureNo), tmp[j,:], 'o-', color = color, label = 'AD', alpha = 0.7)
-            iAD += 1
-            if wholeLabel[i] == 0: #NC
-                for j in range(timeStep):
-                    color = ((iNC+Group2_No)/(Group2_No+Group2_No),0,0)
-                    plotAD, = pyplot.plot(range(featureNo), tmp[j,:], 'o-', color = color, label = 'NC', alpha = 0.7)
-            iNC += 1
+    pyplot.figure(1)
     
-    print (iNC, iAD)
+    for t in range(timeStep):
+        tmp = wholeData[AD_index, :, :]
+        tmp = tmp.reshape(timeStep, featureNo)
+        color = 'r'
+        plotAD, = pyplot.plot(range(featureNo), tmp[t,:], 'o-', color = color, label = 'AD', alpha = 0.7)
+    
+    for t in range(timeStep):
+        tmp = wholeData[NC_index, :, :]
+        tmp = tmp.reshape(timeStep, featureNo)
+        color = 'b'
+        plotNC, = pyplot.plot(range(featureNo), tmp[t,:], 'o-', color = color, label = 'NC', alpha = 0.7)
+    
     pyplot.legend(handles=[plotNC, plotAD], loc = 4)
+
+    pyplot.figure(2)
+    
+    for t in range(timeStep):
+        tmp = wholeData[EMCI_index, :, :]
+        tmp = tmp.reshape(timeStep, featureNo)
+        color = 'g'
+        plotEM, = pyplot.plot(range(featureNo), tmp[t,:], 'o-', color = color, label = 'EMCI', alpha = 0.7)
+    
+    for t in range(timeStep):
+        tmp = wholeData[LMCI_index, :, :]
+        tmp = tmp.reshape(timeStep, featureNo)
+        color = 'm'
+        plotLM, = pyplot.plot(range(featureNo), tmp[t,:], 'o-', color = color, label = 'LMCI', alpha = 0.7)
+    
+
+
+    
+    pyplot.legend(handles=[plotEM, plotLM], loc = 4)
     pyplot.show()
     
     
