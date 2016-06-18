afbcd75c267d2b36dc750af51d16ba708bb29b85
media <zwwang.china@gmail.com>
2016-06-18 15:32:38 -0400

    autoencoder

--- RNN_ADNI_Analysis/AutoEncoder.py
+++ RNN_ADNI_Analysis/AutoEncoder.py
@@ -10,14 +10,26 @@ AutoEncoder: 120-200-100-50-2-50-100-200-120
 @Zhewei
 5/23/2016
 
+Data need to be normalized.
+@Zhewei
+6/18/2016
+
 """
 
 import sys,os
 import gzip
-import cPickle as Pickle
+import pickle as Pickle
 import numpy as np
+from PersonIndependent_LSTM import stackData
 from keras.layers import Dense, Input
 from keras.models import Model
+
+
+totalNo = 76*2
+
+index = [i for i in range(totalNo)]
+
+DataIndex = index
 
 
 def main(args):
@@ -33,50 +45,43 @@ def usage (programm):
     print ("usage: %s data/AD.Pickle.gz data/NC.Pickle.gz ..."%(programm))
 
 def work(fnames):
-    finalResults = list()
-    WholeLabel = list()
-    for fileNo, files in enumerate(fnames):
-        f = gzip.open(files,'rb')
-        data,label = Pickle.load(f)
-        f.close()
-        sampleNo = data.shape[0]
-        WholeLabel.append(label)
-        if fileNo == 0:
-            WholeData = data
-        else:
-            WholeData = np.vstack((WholeData, data))
+    wholeData, wholeLabel = stackData(fnames, DataIndex)
+    print (wholeData.shape)
+    sampleNo = wholeData.shape[0]
+    timeStep = wholeData.shape[1]
+    featureNo = wholeData.shape[2]
 
-        print 'We have', WholeData.shape[0], 'samples.'
-        # just realized the WholeData now is SampleNo*timesteps*FeatureNo, need
-        # to reshape to (SampleNo*timesteps)*FeatureNo
-        WholeData = WholeData.reshape((WholeData.shape[0]*WholeData[1],\
-                                            WholeData.shape[2]))
-        print 'Each sample has', WholeData.shape[1], 'features.'
+    print ('We have', wholeData.shape[0], 'samples.')
+    # just realized the wholeData now is SampleNo*timesteps*FeatureNo, need
+    # to reshape to (SampleNo*timesteps)*FeatureNo
+    wholeData = wholeData.reshape((wholeData.shape[0]*wholeData.shape[1],\
+                                            wholeData.shape[2]))
+    print ('Each sample has', wholeData.shape[1], 'features.')
 
-        '''
-        AutoEncoder
-        120-200-100-50-2-50-100-200-120
-        '''
-        input_data = Input(shape(120,))
-        encode = Dense(200, activation = 'relu')(input_data)
-        encode = Dense(100, activation = 'relu')(encode)
-        encode = Dense(50, activation = 'relu')(encode)
-        encode = Dense(2, activation = 'relu')(encode)
+    '''
+    AutoEncoder
+    120-200-100-50-2-50-100-200-120
+    '''
+    input_data = Input(shape=(120,))
+    encode = Dense(200, activation = 'relu')(input_data)
+    encode = Dense(100, activation = 'relu')(encode)
+    encode = Dense(50, activation = 'relu')(encode)
+    encode = Dense(2, activation = 'relu')(encode)
 
-        decode = Dense(50, activation = 'relu')(encode)
-        decode = Dense(100, activation = 'relu')(decode)
-        decode = Dense(200, activation = 'relu')(decode)
-        decode = Dense(120, activation = 'relu')(decode)
+    decode = Dense(50, activation = 'relu')(encode)
+    decode = Dense(100, activation = 'relu')(decode)
+    decode = Dense(200, activation = 'relu')(decode)
+    decode = Dense(120, activation = 'relu')(decode)
 
-        AutoEncoder = Model(input = input_data, output = decode)
-        encoder = Model(input = input_data, output = encode)
-        AutoEncoder.compile(optimizer = 'adadelta', loss = 'binary_crossentropy')
+    AutoEncoder = Model(input = input_data, output = decode)
+    encoder = Model(input = input_data, output = encode)
+    AutoEncoder.compile(optimizer = 'adadelta', loss = 'binary_crossentropy')
 
-        AutoEncoder.fit(WholeData, WholeData,\
-                        nb.epoch = 100, \
-                        batch_size = 50, \
+    AutoEncoder.fit(wholeData, wholeData,\
+                        nb_epoch = 100, \
+                        batch_size = 100, \
                         shuffle = True)
-        CompressedResult = encoder.predict(WholeData)
+    CompressedResult = encoder.predict(wholeData)
 
 
 
