3f93937e21a2ceb5a06ecd2492b623333c0dab38
media <zwwang.china@gmail.com>
2016-07-24 16:27:46 -0400

    generate pickle

--- RNN_HCP-Q1_Analysis/GenerateSubjPickle.py
+++ RNN_HCP-Q1_Analysis/GenerateSubjPickle.py
@@ -20,18 +20,13 @@ Classes and frames and Label:
 
 all data read or write in ./data/HCP_data/
 
-
+data structure: keys are subjects, values are classes, each class results are in a array, 120(zones)*timeframes.
 
 ******************************************
-1. Set group label.
-2. Set how many sans with noise will generate.
-3. Set postfix name for output pickle. '.pickle.gz' for no-noise, '_noise.pickle.gz' for noise data.
-4. Make sure input correct .txt files.
-5. Add some code to check if value is Nan. It happened for a subject of LMCI. Don't know why. 
-   So now if the file contains Nan, will print the file name.(maybe we can do better.)
+
 ******************************************
 
-Usage:  python3.5 data/AD_Results/AD_Subj*.txt
+Usage:  
 
 Output: an array with the 3D shape SampleNo*FrameNo*FeatureNo and label for each sample
 
@@ -39,3 +34,79 @@ Output: an array with the 3D shape SampleNo*FrameNo*FeatureNo and label for each
 
 
 """
+
+import sys,os
+import gzip
+import pickle as Pickle
+import numpy as np
+import random
+import math
+
+
+#******************************
+#******************************
+IID_List = [100307,103515,103818,111312,114924, \
+117122, 118932, 119833, 120212, 125525, 128632, 130013, \
+131621, 137128, 138231, 142828, 143325, 144226, 149337, \
+150423, 153429, 156637, 159239, 161731, 162329, 167743, \
+172332, 182739, 191437, 192439, 192540, 194140, 197550, \
+199150, 199251, 200614, 201111, 210617, 217429, 249947, \
+250427, 255639, 304020, 307127, 329440, 355542, 499566, \
+530635, 559053, 585862, 611231, 638049, 665254, 672756, \
+685058, 729557, 732243, 792564, 826353, 856766, 859671, \
+861456, 865363, 877168, 889579, 894673, 896778, 896879, \
+901139, 917255, 937160 ]
+WholeData = dict(IID_List)
+print (WholeData.keys())
+label = 0
+postfix = '.pickle.gz'
+#******************************
+#******************************
+
+
+def main(args):
+    if len(args) < 2:
+        usage( args[0] )
+        pass
+    else:
+        work( args[1:] )
+        pass
+    pass
+    
+def usage (programm):
+    print ("usage: %s data/HCP_data/*.txt"%(programm))
+    
+def work(files):
+    for fNo, fi in enumerate(files):
+        print (fi)
+        tmpFile = os.path.basename(fi)
+        print (tmpFile)
+        with open(fi) as f:
+            content = f.readlines()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    main( sys.argv )
+    pass
