1e28fa709d896b3719cf6b1c882a5ce36f60c51a
media <zwwang.china@gmail.com>
2016-09-14 11:49:56 -0400

    C-PAC-master

--- Old_CPAC/scrubbing/__init__.py
+++
@@ -1,7 +0,0 @@
-from .scrubbing import create_scrubbing_preproc, \
-                      get_mov_parameters, \
-                      get_indx
-
-__all__ = ['create_scrubbing_preproc', \
-           'get_mov_parameters', \
-           'get_indx']
