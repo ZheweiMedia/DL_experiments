0d20edf94f7f9440dad021f27a505f4fc87deb8a
media <zwwang.china@gmail.com>
2016-10-20 15:53:55 -0400

    RNN 50_50

--- MatLabCode/VOC2012_Data_Preprocessing.m
+++ MatLabCode/VOC2012_Data_Preprocessing.m
@@ -8,22 +8,23 @@
 %
 %
 
+clear all;
 
-fileFold = fullfile('~/data/VOCdevkit/VOC2012/SegmentationObject/');
-postFix = '*.png';
-cd ~/data/VOCdevkit/VOC2012/SegmentationObject/
-path = '~/data/VOC_Preprocessed_Data/SegmentationObject/50_50/';
+fileFold = fullfile('~/data/VOCdevkit/VOC2012/JPEGImages');
+postFix = '*.jpg';
+cd ~/data/VOCdevkit/VOC2012/JPEGImages/
+path = '~/data/VOC_Preprocessed_Data/JPEGImages/50_50/';
 sizeTo = 50;
 
-dirOutput = dir(fullfile(fileFold, postFix));
-fileNames = {dirOutput.name};
+dirInput = dir(fullfile(fileFold, postFix));
+fileNames = {dirInput.name};
 
 [m,n] = size(fileNames);
 for files = 1:n
     %fileName = strcat(fileNames{1,files},'.jpg');
-    [I, map]  = imread(fileNames{1,files});
-    [I_500, newmap] = imresize(I, map, [sizeTo sizeTo]);
+    I  = imread(fileNames{1,files});
+    I_500 = imresize(I, [sizeTo sizeTo]);
     saveName = strcat(path,fileNames{1,files});
-    imwrite(I_500, newmap,saveName);
+    imwrite(I_500, saveName);
     
 end
