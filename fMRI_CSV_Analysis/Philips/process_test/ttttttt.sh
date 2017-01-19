25010e640ed4d3ffecb08af132fb1fc4ae7ef626
media <zwwang.china@gmail.com>
2017-01-19 12:20:40 -0500

    continue

--- fMRI_CSV_Analysis/Philips/process_test/ttttttt.sh
+++ fMRI_CSV_Analysis/Philips/process_test/ttttttt.sh
@@ -5,6 +5,7 @@
 function processing(){
     fMRI_postFix=$1
     MRI_postFix=$2
+    folder_name=$3
 
     fMRI_ID=${fMRI_postFix:5:6}
     fMRI_timeStamp=${fMRI_postFix:12:19}
@@ -21,8 +22,8 @@ function processing(){
     echo $MRI_timeStamp
 
     ### Step 1: preprocessing of T1 images ###
-    cd /home/medialab/data/ADNI/process_test/MRI/$MRI_postFix
-    dcm2nii -g n -n y /home/medialab/data/ADNI/process_test/MRI/$MRI_postFix
+    cd /home/medialab/data/ADNI/$folder_name/MRI/$MRI_postFix
+    dcm2nii -g n -n y /home/medialab/data/ADNI/$folder_name/MRI/$MRI_postFix
 
     # clean folder
     rm *.dcm
@@ -30,6 +31,19 @@ function processing(){
     ls | grep -v $keywords |xargs rm
     echo `ls *`
     mv *.nii T1.nii
+
+    # remove skull
+    3dSkullStrip -o_ply skullstrip_mask.nii -input T1.nii
+    3dcalc -prefix skullstrip.nii -expr 'a*step(b)' -b skullstrip_mask.nii -a T1.nii
+    ### End of Step 1 ###
+
+    ### Step 2: preprocessing of fMRI images ###
+    cd /home/medialab/data/ADNI/$folder_name/fMRI/$fMRI_postFix
+    dcm2nii -g n -n y /home/medialab/data/ADNI/$folder_name/fMRI/$fMRI_postFix
+    mkdir /home/medialab/data/ADNI/$folder_name/fMRI/$fMRI_postFix/niiFolder
+    cp /home/medialab/data/ADNI/$folder_name/fMRI/$fMRI_postFix/*$fMRI_timeStamp*_*_*.nii \
+       /home/medialab/data/ADNI/$folder_name/fMRI/$fMRI_postFix/niiFolder
+    rm /home/medialab/data/ADNI/$folder_name/fMRI/$fMRI_postFix/*
     
 }
 
@@ -39,9 +53,10 @@ function processing(){
 
 fMRI_imageIDs=(`cat $1`)
 MRI_imageIDs=(`cat $2`)
+folder_name=$3
 
 
-Core=2
+Core=3
 ID_Number=5
 range_array=()
 
@@ -55,5 +70,5 @@ for i in ${range_array[@]};
 do
     ((c=c%Core)); ((c++==0)) && wait
     echo $i
-    processing ${fMRI_imageIDs[i]} ${MRI_imageIDs[i]}& 
+    processing ${fMRI_imageIDs[i]} ${MRI_imageIDs[i]} $folder_name& 
 done
