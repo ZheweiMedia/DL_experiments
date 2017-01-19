48c9d2f10c78bba70c21c457790ab121457085d8
media <zwwang.china@gmail.com>
2017-01-19 11:43:30 -0500

    test AFNI

--- fMRI_CSV_Analysis/Philips/Step05_Add_time_stamp.sh
+++ fMRI_CSV_Analysis/Philips/Step05_Add_time_stamp.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 
 # use for rename fMRI folders
+# usage: bash Step05_Add_time_stamp.sh Clean_fMRI_ImageID Clean_MRI_ImageID 
 
 
 # first, load fMRI image IDs
 fMRI_IDs=`cat ~/Zhewei/fMRI_CSV_Analysis/Philips/$1`
+MRI_IDs=`cat ~/Zhewei/fMRI_CSV_Analysis/Philips/$2`
 
 # second, go to ADNI folder to read xml files
 cd /home/medialab/data/ADNI/Philips/ADNI
 fMRI_XMLS=`find ./ -name "*Resting_State_fMRI*.xml"`
+MRI_XMLS=`find ./ -name "*MPRAGE*.xml"`
 
 for X in $fMRI_XMLS;
 do
@@ -26,6 +29,26 @@ do
     case "${fMRI_IDs[@]}" in
         *$iid*)
             mv /home/medialab/data/ADNI/Philips/fMRI/fMRI_${iid} /home/medialab/data/ADNI/Philips/fMRI/fMRI_${iid}_${timeFilt}
+    esac
+done
+
+
+for X in $MRI_XMLS;
+do
+    iid=`xmllint --xpath '//project/subject/study/series/imagingProtocol/imageUID/text()' $X`
+    # IMPORTANT:
+    time=`xmllint --xpath '//project/subject/study/series/dateAcquired/text()' $X`
+
+    # string of time in files
+    timeYear=${time:0:4}
+    timeMonthDay=${time:5:5}
+    timeMonth=${timeMonthDay:0:2}
+    timeDay=${timeMonthDay:3:4}
+    timeFilt=$timeYear$timeMonth$timeDay
+
+    case "${MRI_IDs[@]}" in
+        *$iid*)
+            mv /home/medialab/data/ADNI/Philips/MRI/MRI_${iid} /home/medialab/data/ADNI/Philips/MRI/MRI_${iid}_${timeFilt}
     esac
 done
 
--- fMRI_CSV_Analysis/Philips/process_test/cp_process_test.sh
+++ fMRI_CSV_Analysis/Philips/process_test/cp_process_test.sh
@@ -8,7 +8,7 @@ mkdir ./fMRI
 
 cd /home/medialab/data/ADNI/Philips/MRI/
 
-cp -r MRI_238627 MRI_303066 MRI_240812 MRI_304793 MRI_371991 ../../process_test/MRI/
+cp -r MRI_238627_20110602 MRI_303066_20120510 MRI_240812_20110616 MRI_304793_20120515 MRI_371991_20130510 ../../process_test/MRI/
 
 cd /home/medialab/data/ADNI/Philips/fMRI/
 
