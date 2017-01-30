ee6a1dbffe7e6c4f0b1084c518e3884ee3f2ca98
media <zwwang.china@gmail.com>
2017-01-30 13:58:12 -0500

    Incomplete list

--- fMRI_CSV_Analysis/SIEMENS/Step11_find_uncomplete_brain.py
+++ fMRI_CSV_Analysis/SIEMENS/Step11_find_uncomplete_brain.py
@@ -11,6 +11,9 @@ import os
 import numpy
 import nibabel as nib
 
+in_complete_brain = list()
+in_complete_rate = 0.1
+
 template = nib.load('/home/medialab/data/template/AAL2.nii')
 template_data = template.get_data()
 #print (template.shape)
@@ -33,11 +36,31 @@ for zone_no, index in enumerate(index_list):
 #print(len(zone_index_list))
 #print(template_data[zone_index_list[1]])
 
+folder_names = os.listdir('/home/medialab/data/ADNI/SIEMENS/fMRI/')
+base_folder = '/home/medialab/data/ADNI/SIEMENS/fMRI/'
+
+invalid_list = list()
+
+for folder in folder_names:
+    image_path = base_folder+folder+'/result_image/fMRI_removenoise.nii'
+    img = nib.load(image_path)
+    img_data = img.get_data()
+
+    for zone_no, index in enumerate(index_list):
+        zone_data = img_data[zone_index_list[zone_no]]
+        total_pixel = 1
+        for shape in list(zone_data.shape):
+            total_pixel = total_pixel*shape
+
+        zero_in_zone = numpy.where(zone_data == 0)
+        
+        zero_percentage = (len(zero_in_zone[0]))/(total_pixel)
+        if zero_percentage > in_complete_rate or zero_percentage == in_complete_rate:
+            invalid_list.append(folder)
+            
 
 
 
-
-img = nib.load('/home/medialab/data/ADNI/SIEMENS/fMRI/fMRI_196079_20101008/result_image/fMRI_removenoise.nii')
 
 #print (img.shape)
 # (91, 109, 91, 100)
