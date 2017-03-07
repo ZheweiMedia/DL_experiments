5f789afdf6a7950d59bd3ac4919eb4881921acb3
media <zwwang.china@gmail.com>
2017-03-07 15:43:13 -0500

    HCP RELATIONS

--- fMRI_CSV_Analysis/HCP/Step10_prepare_data.sh
+++ fMRI_CSV_Analysis/HCP/Step10_prepare_data.sh
@@ -13,8 +13,8 @@ for subjects in ${subjects_list[@]};
 do
     echo $subjects
     mkdir ~/data/HCP-Q1/fMRI/$subjects
-    cp /media/medialab/HCP-Q1/$subjects/unprocessed/3T/tfMRI_EMOTION_LR/${subjects}_3T_tfMRI_EMOTION_LR.nii.gz ~/data/HCP-Q1/fMRI/$subjects/
-    cp -r /media/medialab/HCP-Q1/$subjects/unprocessed/3T/tfMRI_EMOTION_LR/LINKED_DATA/EPRIME/EVs ~/data/HCP-Q1/fMRI/$subjects/
+    cp /media/medialab/HCP-Q1/$subjects/unprocessed/3T/tfMRI_RELATIONAL_LR/${subjects}_3T_tfMRI_RELATIONAL_LR.nii.gz ~/data/HCP-Q1/fMRI/$subjects/
+    cp -r /media/medialab/HCP-Q1/$subjects/unprocessed/3T/tfMRI_RELATIONAL_LR/LINKED_DATA/EPRIME/EVs ~/data/HCP-Q1/fMRI/$subjects/
 
     mkdir ~/data/HCP-Q1/MRI/$subjects
     cp /media/medialab/HCP-Q1/$subjects/unprocessed/3T/T1w_MPR1/${subjects}_3T_T1w_MPR1.nii.gz ~/data/HCP-Q1/MRI/$subjects
